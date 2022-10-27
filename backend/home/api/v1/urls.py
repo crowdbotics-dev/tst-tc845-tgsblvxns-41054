c043a9fbe952ae3e3aa8fc7379687a45b56b4557
Crowdbotics <engineering@crowdbotics.com>
2022-10-27 00:34:25 +0000

    Update models

--- backend/home/api/v1/urls.py
+++ backend/home/api/v1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .viewsets import AddressigbxyrdxtaViewSet
 
 from home.api.v1.viewsets import (
     SignupViewSet,
@@ -9,6 +10,7 @@ from home.api.v1.viewsets import (
 router = DefaultRouter()
 router.register("signup", SignupViewSet, basename="signup")
 router.register("login", LoginViewSet, basename="login")
+router.register('addressigbxyrdxta', AddressigbxyrdxtaViewSet )
 
 urlpatterns = [
     path("", include(router.urls)),
--- backend/home/api/v1/viewsets.py
+++ backend/home/api/v1/viewsets.py
@@ -1,3 +1,7 @@
+from rest_framework import viewsets
+from home.models import Addressigbxyrdxta
+from .serializers import AddressigbxyrdxtaSerializer
+from rest_framework import authentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.authtoken.models import Token
@@ -28,3 +32,8 @@ class LoginViewSet(ViewSet):
         token, created = Token.objects.get_or_create(user=user)
         user_serializer = UserSerializer(user)
         return Response({"token": token.key, "user": user_serializer.data})
+
+class AddressigbxyrdxtaViewSet(viewsets.ModelViewSet):
+    serializer_class = AddressigbxyrdxtaSerializer
+    authentication_classes = (authentication.SessionAuthentication, authentication.TokenAuthentication)
+    queryset = Addressigbxyrdxta.objects.all()
